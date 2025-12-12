5d4177f6d9cb0f14e3aadf93f8a7a977ca2af65e
aaditya01sehgal <aadityasehgal567@gmail.com>
2025-12-12 23:40:58 +0530

    Add the Backend Folder Structure:

---
+++ CalmQuest_Backend/src/main/java/com/calmquest/calmquest_backend/CalmquestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calmquest.calmquest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmquestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalmquestBackendApplication.class, args);
+    }
+
+}
